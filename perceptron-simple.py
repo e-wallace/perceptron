3f00d23fe1384203ea8fa90e839a289e6f35d756
e-wallace <44043492+e-wallace@users.noreply.github.com>
2020-12-29 17:42:01 -0500

    Update and rename A1_P1.py to perceptron-simple.py

--- perceptron-simple.py
+++ perceptron-simple.py
@@ -2,7 +2,7 @@
 Queen's University, Fall 2020
 Course: COGS 400 - Neural and Genetic Computing
 Assignment 1 - Perceptron Part A - Simple Feedback Learning
-Student Number: 20062694
+Student Number: ********
 '''
 import numpy as np
 import pandas as pd
